559ac73864fa8df2b56ccde7b8860f27bfe8b75b
Renan <renan.carli@hotmail.com>
2025-04-16 21:54:34 -0300

    Primeiro commit

---
+++ hackathon-carteira/src/main/java/br/com/fiap/hackathon_carteira/HackathonCarteiraApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.hackathon_carteira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackathonCarteiraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackathonCarteiraApplication.class, args);
+	}
+
+}
---
+++ hackathon-carteira/src/test/java/br/com/fiap/hackathon_carteira/HackathonCarteiraApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.hackathon_carteira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackathonCarteiraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
